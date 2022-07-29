f4f153991cd0f78cdac7abbe916cc9fbb3b857e8
NovumChaldea <deusable@gmail.com>
2022-07-29 05:42:15 -0700

    finished step 7

--- tutorial/app.js
+++ tutorial/app.js
@@ -7,6 +7,9 @@
 //process - info about env where the program is being executed
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade')
+
 
 sayHi('Chaldea')
 sayHi(names.john)
