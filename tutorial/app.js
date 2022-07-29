534f33a54936d5559f6e57a07ca47c28882f8298
NovumChaldea <deusable@gmail.com>
2022-07-29 04:54:49 -0700

    finished intro

--- tutorial/app.js
+++ tutorial/app.js
@@ -5,3 +5,9 @@
 //require - funtion to use modules
 //module - info about current module
 //process - info about env where the program is being executed
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log("hello world")
+}, 5000);
