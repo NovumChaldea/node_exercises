d1fc0ea2e376c4a3b6a4af09c1641425552db98f
NovumChaldea <deusable@gmail.com>
2022-08-09 22:56:45 -0700

    fs async module

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,28 +1,13 @@
-const { readFile, writeFile } = require('fs')
+const {readFileSync, writeFilesSync, writeFile, writeFileSync} = require('fs');
+console.log("Start");
 
-console.log('start')
-readFile('../content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  const first = result
-  readFile('../content/second.txt', 'utf8', (err, result) => {
-    if (err) {
-      console.log(err)
-      return
-    }
-    const second = result
-    writeFile(
-      '../content/result-async.txt',
-      `Here is the result : ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-        console.log(result)
-      }
-    )
-  })
-})
+const first = readFileSync('../content/first.txt', 'utf8');
+const second = readFileSync('../content/second.txt', 'utf8');
+
+writeFileSync(
+  '../content/result-sync.txt',
+  `Here is the result: ${first}, ${second}`,
+  {flag:'a'}
+)
+
+console.log('done')
