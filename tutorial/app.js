aae23e281df679d3ac1834a7c05bfebff6cb94ec
NovumChaldea <deusable@gmail.com>
2022-08-22 17:15:19 -0700

    Progress in Node course

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,14 +1,17 @@
-const {readFileSync, writeFilesSync, writeFile, writeFileSync} = require('fs');
-console.log("Start");
+const http = require('http');
 
-const first = readFileSync('../content/first.txt', 'utf8');
-const second = readFileSync('../content/second.txt', 'utf8');
+const server = http.createServer((req, res)=>{
+  if(req.url === '/'){
+    res.end('Welcome to our own page')
+  }
+  if(req.url ==='/about'){
+    res.end('Here is our short story')
+  }
+  res.end(`
+  <h1>Ooops!</h1>
+  <p>We can't seem to fin the page you are looking for </p>
+  <a href="/">Back home</a>
+  `)
+})
 
-writeFileSync(
-  '../content/result-sync.txt',
-  `Here is the result: ${first}, ${second}`,
-  {flag:'a'}
-)
-
-console.log('done with this task')
-console.log('starting the next one')
+server.listen(5000)
